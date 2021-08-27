0125ef4697b0a1cd292f6d7d1b231420f819b426
Hung Duong Quang <qhungg289@protonmail.com>
2021-08-27 22:17:14 +0700

    Change variable

--- test/ship.test.js
+++ test/ship.test.js
@@ -1,18 +1,18 @@
 import Ship from "../src/ship";
 
 describe("check ship status", () => {
-	const newShip = Ship(3);
+	const testShip = Ship(3);
 
 	test("is the ship get hit", () => {
-		newShip.hit(2);
-		expect(newShip.shipParts).toEqual([false, false, true]);
-		expect(newShip.shipParts[2]).toBe(true);
+		testShip.hit(2);
+		expect(testShip.shipParts).toEqual([false, false, true]);
+		expect(testShip.shipParts[2]).toBe(true);
 	});
 
 	test("is the ship sunk", () => {
-		newShip.hit(0);
-		newShip.hit(1);
-		newShip.hit(2);
-		expect(newShip.isSunk()).toBe(true);
+		testShip.hit(0);
+		testShip.hit(1);
+		testShip.hit(2);
+		expect(testShip.isSunk()).toBe(true);
 	});
 });
