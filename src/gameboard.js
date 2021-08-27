5bd0e13d06f18ee08780eeb0ac7411b02a08df99
Hung Duong Quang <qhungg289@protonmail.com>
2021-08-27 22:16:47 +0700

    Fix ship overlap and board overflow

--- src/gameboard.js
+++ src/gameboard.js
@@ -3,6 +3,7 @@ import Ship from "./ship";
 function Gameboard() {
 	let board = [];
 	let isAllSunk = false;
+	let missedShot = [];
 
 	// Set width and height of the board then populate it with null values
 	// // Height
@@ -17,22 +18,36 @@ function Gameboard() {
 		}
 	}
 
+	/* Each parts of a ship need to have an index so it can
+	determine which part has been hit to respond correctly */
 	function placeShip(x, y, shipLength, orientation) {
 		const newShip = Ship(shipLength);
 		if (orientation == "horizontal" && x + shipLength <= 10) {
 			for (let i = 0; i < shipLength; i++) {
-				board[y][x] = newShip.shipParts[i];
+				if (board[y][x + i] != null) return;
+			}
+			for (let i = 0; i < shipLength; i++) {
+				board[y][x] = newShip.shipParts[i]; // Change this
 				x++;
 			}
 		} else if (orientation == "vertical" && y + shipLength <= 10) {
 			for (let i = 0; i < shipLength; i++) {
-				board[y][x] = newShip.shipParts[i];
+				if (board[y + i][x] != null) return;
+			}
+			for (let i = 0; i < shipLength; i++) {
+				board[y][x] = newShip.shipParts[i]; // And this
 				y++;
 			}
 		}
 	}
 
-	return { board, isAllSunk, placeShip };
+	function receiveAttack(x, y) {
+		if (board[y][x] == null) {
+			missedShot.push({ x: x, y: y });
+		}
+	}
+
+	return { board, missedShot, isAllSunk, placeShip, receiveAttack };
 }
 
 export default Gameboard;
