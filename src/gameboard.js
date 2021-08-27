ef8ea809618da6fd0b57fdf88794106ba094a5aa
Hung Duong Quang <qhungg289@protonmail.com>
2021-08-27 10:23:53 +0700

    Prevent board overflow

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,12 +2,15 @@ import Ship from "./ship";
 
 function Gameboard() {
 	let board = [];
+	let isAllSunk = false;
 
 	// Set width and height of the board then populate it with null values
+	// // Height
 	for (let i = 0; i < 10; i++) {
 		board.push([]);
 	}
 
+	// // Width
 	for (let i = 0; i < board.length; i++) {
 		for (let j = 0; j < 10; j++) {
 			board[i].push(null);
@@ -16,20 +19,20 @@ function Gameboard() {
 
 	function placeShip(x, y, shipLength, orientation) {
 		const newShip = Ship(shipLength);
-		if (orientation == "horizontal") {
-			for (let i = 0; i < newShip.shipParts.length; i++) {
+		if (orientation == "horizontal" && x + shipLength <= 10) {
+			for (let i = 0; i < shipLength; i++) {
 				board[y][x] = newShip.shipParts[i];
 				x++;
 			}
-		} else if (orientation == "vertical") {
-			for (let i = 0; i < newShip.shipParts.length; i++) {
+		} else if (orientation == "vertical" && y + shipLength <= 10) {
+			for (let i = 0; i < shipLength; i++) {
 				board[y][x] = newShip.shipParts[i];
 				y++;
 			}
 		}
 	}
 
-	return { board, placeShip };
+	return { board, isAllSunk, placeShip };
 }
 
 export default Gameboard;
