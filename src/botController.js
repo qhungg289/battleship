e7fcb3a396352728cfc0742c0b2540243dc0ad33
Hung Duong Quang <qhungg289@protonmail.com>
2021-08-31 20:45:43 +0700

    Refactor

--- src/botController.js
+++ src/botController.js
@@ -4,10 +4,14 @@ function autoAttack(player) {
 	const column = Math.floor(Math.random() * player.board.length);
 	const row = Math.floor(Math.random() * player.board.length);
 
-	player.receiveAttack(column, row);
+	if (alreadyHit.includes({ x: column, y: row })) {
+		autoAttack(player);
+	} else {
+		player.receiveAttack(column, row);
+		alreadyHit.push({ x: column, y: row });
+	}
 }
 
-// TODO: implement this
 function autoPlaceShip(bot) {}
 
 export { autoAttack, autoPlaceShip };
--- src/gameboard.js
+++ src/gameboard.js
@@ -2,7 +2,6 @@ import Ship from "./ship";
 
 function Gameboard() {
 	let board = [];
-	let missedShot = [];
 
 	for (let i = 0; i < 10; i++) {
 		board.push([]);
@@ -10,23 +9,22 @@ function Gameboard() {
 
 	for (let i = 0; i < board.length; i++) {
 		for (let j = 0; j < 10; j++) {
-			board[i].push(null);
+			board[i].push({ hit: false });
 		}
 	}
 
-	// Maybe the "hit" property is unnecessary?
 	function placeShip(x, y, shipLength, orientation) {
 		const ship = Ship(shipLength);
 		if (orientation == "horizontal" && x + shipLength <= 10) {
 			for (let i = 0; i < shipLength; i++) {
-				if (board[y][x + i] != null) return;
+				if ("ship" in board[y][x + i] && "part" in board[y][x + i]) return;
 			}
 			for (let i = 0; i < shipLength; i++) {
 				board[y][x + i] = { ship: ship, part: i, hit: false };
 			}
 		} else if (orientation == "vertical" && y + shipLength <= 10) {
 			for (let i = 0; i < shipLength; i++) {
-				if (board[y + i][x] != null) return;
+				if ("ship" in board[y + i][x] && "part" in board[y + i][x]) return;
 			}
 			for (let i = 0; i < shipLength; i++) {
 				board[y + i][x] = { ship: ship, part: i, hit: false };
@@ -35,11 +33,11 @@ function Gameboard() {
 	}
 
 	function receiveAttack(x, y) {
-		if (board[y][x] != null) {
+		if ("ship" in board[y][x] && "part" in board[y][x]) {
 			board[y][x].ship.hit(board[y][x].part);
 			board[y][x].hit = true;
 		} else {
-			missedShot.push({ x: x, y: y });
+			board[y][x].hit = true;
 		}
 	}
 
@@ -47,7 +45,7 @@ function Gameboard() {
 		let occupiedCells = [];
 		board.forEach((row) => {
 			row.forEach((cell) => {
-				if (cell != null) {
+				if ("ship" in cell && "part" in cell) {
 					occupiedCells.push(cell);
 				}
 			});
@@ -55,7 +53,7 @@ function Gameboard() {
 		return occupiedCells.every((cell) => cell.ship.isSunk());
 	}
 
-	return { board, missedShot, placeShip, receiveAttack, isAllSunk };
+	return { board, placeShip, receiveAttack, isAllSunk };
 }
 
 export default Gameboard;
