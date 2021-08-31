8fb8a583d1f592fb39835e789970813ea5991f2f
Hung Duong Quang <qhungg289@protonmail.com>
2021-08-31 10:52:08 +0700

    Refactor

--- src/gameboard.js
+++ src/gameboard.js
@@ -4,18 +4,17 @@ function Gameboard() {
 	let board = [];
 	let missedShot = [];
 
-	// // Height
 	for (let i = 0; i < 10; i++) {
 		board.push([]);
 	}
 
-	// // Width
 	for (let i = 0; i < board.length; i++) {
 		for (let j = 0; j < 10; j++) {
 			board[i].push(null);
 		}
 	}
 
+	// Maybe the "hit" property is unnecessary?
 	function placeShip(x, y, shipLength, orientation) {
 		const ship = Ship(shipLength);
 		if (orientation == "horizontal" && x + shipLength <= 10) {
@@ -23,21 +22,22 @@ function Gameboard() {
 				if (board[y][x + i] != null) return;
 			}
 			for (let i = 0; i < shipLength; i++) {
-				board[y][x + i] = { ship: ship, partIndex: i };
+				board[y][x + i] = { ship: ship, part: i, hit: false };
 			}
 		} else if (orientation == "vertical" && y + shipLength <= 10) {
 			for (let i = 0; i < shipLength; i++) {
 				if (board[y + i][x] != null) return;
 			}
 			for (let i = 0; i < shipLength; i++) {
-				board[y + i][x] = { ship: ship, partIndex: i };
+				board[y + i][x] = { ship: ship, part: i, hit: false };
 			}
 		}
 	}
 
 	function receiveAttack(x, y) {
 		if (board[y][x] != null) {
-			board[y][x].ship.hit(board[y][x].partIndex);
+			board[y][x].ship.hit(board[y][x].part);
+			board[y][x].hit = true;
 		} else {
 			missedShot.push({ x: x, y: y });
 		}
