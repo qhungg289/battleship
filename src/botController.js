77d53e3b1212b759eb0b503c95ce8dccb2531ee7
Hung Duong Quang <qhungg289@protonmail.com>
2021-09-01 20:59:58 +0700

    Fix autoAttack()

--- src/botController.js
+++ src/botController.js
@@ -1,10 +1,14 @@
 let alreadyHit = [];
 
 function autoAttack(player) {
-	const column = Math.floor(Math.random() * player.board.length);
-	const row = Math.floor(Math.random() * player.board.length);
+	let column = Math.floor(Math.random() * player.board.length);
+	let row = Math.floor(Math.random() * player.board.length);
 
-	if (alreadyHit.includes({ x: column, y: row })) {
+	if (
+		alreadyHit.some(
+			(coordinate) => coordinate.x == column && coordinate.y == row
+		)
+	) {
 		autoAttack(player);
 	} else {
 		player.receiveAttack(column, row);
