c5e18279d242583a1b9ed135dc87ac0bb7755ae3
Hung Duong Quang <qhungg289@protonmail.com>
2021-09-05 20:31:37 +0700

    Update botController.js

--- src/botController.js
+++ src/botController.js
@@ -2,52 +2,78 @@ import Gameboard from "./Gameboard";
 
 export default class BotController extends Gameboard {
 	#alreadyHit = [];
+
 	#coordinatesRandomizer() {
 		const x = Math.floor(Math.random() * 10);
 		const y = Math.floor(Math.random() * 10);
 		return [x, y];
 	}
+
+	#orientationRandomizer() {
+		const shipOrientations = ["vertical", "horizontal"];
+		return shipOrientations[
+			Math.floor(Math.random() * shipOrientations.length)
+		];
+	}
+
 	autoAttack(player) {
-		const [x, y] = this.#coordinatesRandomizer();
-		if (
+		let [x, y] = this.#coordinatesRandomizer();
+		while (
 			this.#alreadyHit.some(
 				(coordinate) => coordinate.x == x && coordinate.y == y
 			)
 		) {
-			this.autoAttack(player);
-		} else {
-			player.receiveAttack(x, y);
-			this.#alreadyHit.push({ x: x, y: y });
+			[x, y] = this.#coordinatesRandomizer();
+		}
+		player.receiveAttack(x, y);
+		this.#alreadyHit.push({ x: x, y: y });
+	}
+
+	#autoPlaceShipsCells(shipLength) {
+		let [x, y] = this.#coordinatesRandomizer();
+		let orientation = this.#orientationRandomizer();
+		try {
+			if (orientation == "horizontal") {
+				while (x + shipLength > 10) {
+					[x, y] = this.#coordinatesRandomizer();
+				}
+				for (let i = 0; i < shipLength; i++) {
+					while (
+						this.board[y] == undefined ||
+						this.board[y][x + i] == undefined ||
+						"ship" in this.board[y][x + i] ||
+						"part" in this.board[y][x + i]
+					) {
+						[x, y] = this.#coordinatesRandomizer();
+					}
+				}
+				this.placeShip(x, y, shipLength, orientation);
+			} else if (orientation == "vertical") {
+				while (y + shipLength > 10) {
+					[x, y] = this.#coordinatesRandomizer();
+				}
+				for (let i = 0; i < shipLength; i++) {
+					while (
+						this.board[y + i] == undefined ||
+						this.board[y + i][x] == undefined ||
+						"ship" in this.board[y + i][x] ||
+						"part" in this.board[y + i][x]
+					) {
+						[x, y] = this.#coordinatesRandomizer();
+					}
+				}
+				this.placeShip(x, y, shipLength, orientation);
+			}
+		} catch (error) {
+			console.error(error);
+			this.#autoPlaceShipsCells(shipLength);
 		}
 	}
-	#autoPlaceShipsCells(length, orientation) {
-		const [x, y] = this.#coordinatesRandomizer();
-		if (orientation == "horizontal" && x + length <= 10) {
-			for (let i = 0; i < length; i++) {
-				if ("ship" in this.board[y][x + i] && "part" in this.board[y][x + 1]) {
-					this.#autoPlaceShipsCells(length, orientation);
-				}
-			}
-			this.placeShip(x, y, length, orientation);
-		} else if (orientation == "vertical" && y + length <= 10) {
-			for (let i = 0; i < length; i++) {
-				if ("ship" in this.board[y + i][x] && "part" in this.board[y + i][x]) {
-					this.#autoPlaceShipsCells(length, orientation);
-				}
-			}
-			this.placeShip(x, y, length, orientation);
-		} else {
-			this.#autoPlaceShipsCells(length, orientation);
-		}
-	}
+
 	autoPlaceShips() {
 		const shipUnits = [5, 4, 3, 2, 2, 1, 1];
-		const shipOrientations = ["vertical", "horizontal"];
 		for (let i = 0; i < shipUnits.length; i++) {
-			this.#autoPlaceShipsCells(
-				shipUnits[i],
-				shipOrientations[Math.floor(Math.random() * shipOrientations.length)]
-			);
+			this.#autoPlaceShipsCells(shipUnits[i]);
 		}
 	}
 }
